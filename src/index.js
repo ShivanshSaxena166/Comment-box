0846815130951da24ef77a773de14375f759c9e5
unknown <shivanshkumar166@gmail.com>
2020-10-15 01:16:40 +0530

    approval card children

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import ApprovalCard from './ApprovalCard';
 const App =()=>{
     return (
         <div className="ui container comments">
-<ApprovalCard />
+<ApprovalCard>
 
 <CommentDetails author="Shivansh" timeAgo="4 hours ago" content="Nice Blog post" />
+</ApprovalCard>
 <CommentDetails author="Dev1" timeAgo="8 hours ago"  content="I like the pic"/>
 <CommentDetails author="Dev2" timeAgo="17 hours ago" content="Love"/>
 <CommentDetails author="Dev2" timeAgo="yesterday" content="woah!!!"/>
