6233f8088fef74f7e7f352d3f63e20f9a98ea536
unknown <shivanshkumar166@gmail.com>
2020-10-14 23:44:56 +0530

    approval card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetails from './CommentDetails'
+import ApprovalCard from './ApprovalCard';
 const App =()=>{
     return (
         <div className="ui container comments">
-
+<ApprovalCard />
 
 <CommentDetails author="Shivansh" timeAgo="4 hours ago" content="Nice Blog post" />
 <CommentDetails author="Dev1" timeAgo="8 hours ago"  content="I like the pic"/>
